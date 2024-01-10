ffc93f35c5f61b5a1847de198464ca53058dbf3a
nicolas772 <nicolas.arayau@sansano.usm.cl>
2024-01-10 16:58:29 -0300

    modificacion de comportamiento en configuraciones de notificaciones

--- src/renders/configuraciones.js
+++ src/renders/configuraciones.js
@@ -3,6 +3,23 @@ document.addEventListener('DOMContentLoaded', async () => {
     const divGuardarBtn = document.getElementById("divGuardarBtn");
     const loadingSpinner = document.getElementById("loadingSpinner");
     const guardarCambiosBtn = document.getElementById('guardarCambiosBtn');
+    const radioTiempo = document.getElementById("inputTiempo")
+    const radioReconocimiento = document.getElementById("inputReconocimiento")
+    const selectReconocimiento = document.getElementById("selectIntervaloReconocimiento")
+    const selectTiempo = document.getElementById("selectIntervaloTiempo")
+
+    radioReconocimiento.addEventListener('change', function () {
+        selectReconocimiento.disabled = false;
+        selectTiempo.disabled = true;
+    });
+
+    radioTiempo.addEventListener('change', function () {
+        selectReconocimiento.disabled = true;
+        selectTiempo.disabled = false;
+    });
+    
+
+
     guardarCambiosBtn.addEventListener('click', async () => {
         divGuardarBtn.classList.add('d-none')
         loadingSpinner.classList.remove('d-none')
